bec619ad8883422420eae1e000a89e78c5d57453
llHoYall <hoya128@gmail.com>
2019-05-03 22:38:30 +0900

    Update "urllib" in playground

--- playground/urllib_playground.py
+++ playground/urllib_playground.py
@@ -1,25 +1,53 @@
 import sys
 import io
-import urllib.request as dw
+import urllib.request as req
+from urllib.parse import urlparse
+from urllib.parse import urlencode
 
-sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
-sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding="utf-8")
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding="utf-8")
 
-imgURL = "http://post.phinf.naver.net/20160621_169/1466482468068lmSHj_JPEG/If7GeIbOPZuYwI-GI3xU7ENRrlfI.jpg"
-htmlURL = "http://google.com"
+# imgURL = "http://post.phinf.naver.net/20160621_169/1466482468068lmSHj_JPEG/If7GeIbOPZuYwI-GI3xU7ENRrlfI.jpg"
+# req.urlretrieve(imgURL, savePath1)
+# f = req.urlopen(imgURL)
+# savePath = "./test.jpg"
+# saveFile = open(savePath, "wb")
+# saveFile.write(f.read())
+# saveFile.close()
 
-savePath1 = "./test1.jpg"
-savePath2 = "./index.html"
+# googleURL = "http://google.com"
+# req.urlretrieve(googleURL, savePath2)
+# f = req.urlopen(googleURL).read()
+# savePath = "./test.html"
+# with open(savePath, "wb") as saveFile:
+#     saveFile.write(f)
 
-# dw.urlretrieve(imgURL, savePath1)
-f = dw.urlopen(imgURL)
-saveFile1 = open(savePath1, 'wb')
-saveFile1.write(f.read())
-saveFile1.close()
+# encarURL = "http://www.encar.com"
+# mem = req.urlopen(encarURL)
+# print("geturl: ", mem.geturl())
+# print("status: ", mem.status)
+# print("headers: ", mem.getheaders())
+# print("info: ", mem.info())
+# print("code: ", mem.getcode())
+# print("read: ", mem.read(50 ).decode("utf-8"))
+# print(urlparse(encarURL))
 
-# dw.urlretrieve(htmlURL, savePath2)
-f2 = dw.urlopen(htmlURL).read()
-with open(savePath2, 'wb') as saveFile2:
-	saveFile2.write(f2)
+# API = "https://api.ipify.org"
+# values = {
+# 	'format': 'json'
+# }
+# params = urlencode(values)
+# url = API + "?" + params
+# reqData = req.urlopen(url).read().decode('utf-8')
+# print('before: ', values)
+# print('after: ', params)
+# print("URL: ", url)
+# print("data: ", reqData)
+
+# API = "http://www.mois.go.kr/gpms/view/jsp/rss/rss.jsp"
+# values = {"ctxCd": "1001"}
+# params = urlencode(values)
+# url = API + "?" + params
+# print("URL: ", url)
 
 print("Complete!")
