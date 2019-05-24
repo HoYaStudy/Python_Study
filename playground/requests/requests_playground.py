95ced72258037e34c665611a3154982de58838db
llHoYall <hoya128@gmail.com>
2019-05-24 22:09:27 +0900

    Update "requests" in playground

--- playground/requests/requests_playground.py
+++ playground/requests/requests_playground.py
@@ -17,13 +17,43 @@ import requests
 #     print(r.encoding)
 #     print(r.content)
 
-url = "http://httpbin.org/stream/20"
-with requests.Session() as s:
-    r = s.get(url, stream=True)
-    if r.encoding is None:
-        r.encoding = "utf-8"
+# url = "http://httpbin.org/stream/20"
+# with requests.Session() as s:
+#     r = s.get(url, stream=True)
+#     if r.encoding is None:
+#         r.encoding = "utf-8"
 
-    for line in r.iter_lines(decode_unicode=True):
-        l = json.loads(line)
-        for k in l.keys():
-            print("key: ", k, ", values: ", l[k])
+#     for line in r.iter_lines(decode_unicode=True):
+#         l = json.loads(line)
+#         for k in l.keys():
+#             print("key: ", k, ", values: ", l[k])
+
+# url = "https://api.github.com/events"
+# r = requests.get(url)
+# r.raise_for_status()
+
+# jar = requests.cookies.RequestsCookieJar()
+# jar.set("name", "kim", domain="httpbin.org", path="/cookies")
+# r = requests.get("http://httpbin.org/cookies", cookies=jar)
+# r.raise_for_status()
+# print(r.text)
+
+# r = requests.get("https://github.com", timeout=3)
+# print(r.text)
+
+# jar = requests.cookies.RequestsCookieJar()
+# jar.set("age", "18", domain="httpbin.org", path="/cookies")
+# r = requests.post("http://httpbin.org/post", data={"name": "kim"}, cookies=jar)
+# print(r.text)
+
+# payload1 = {"key1": "value1", "key2": "value2"}
+# payload2 = (("key1", "value1"), ("key2", "value2"))
+# payload3 = {"key1": "value1"}
+# r = requests.post("http://httpbin.org/post", data=payload1)
+# r = requests.post("http://httpbin.org/post", data=payload2)
+# r = requests.post("http://httpbin.org/post", data=json.dumps(payload3))
+# r = requests.put("http://httpbin.org/put", data=payload1)
+# r = requests.delete("http://httpbin.org/delete")
+# r = requests.put("https://jsonplaceholder.typicode.com/posts/1", data=payload1)
+# r = requests.delete("https://jsonplaceholder.typicode.com/posts/1")
+# print(r.text)
