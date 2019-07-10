3a0a4ba6574a1000c4c04208f89e47be4bdd6096
llHoYall <hoya128@gmail.com>
2019-07-10 21:00:01 +0900

    Update youtube_player to PyQt

--- pyqt/youtube_player/main.py
+++ pyqt/youtube_player/main.py
@@ -3,8 +3,9 @@ import datetime
 import re
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
-from PyQt5.QtCore import pyqtSlot, pyqtSignal
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QUrl
 # from PyQt5 import uic
+import pytube
 
 from lib.YouViewerLayout import Ui_MainWindow
 from lib.AuthDialog import AuthDialog
@@ -18,6 +19,9 @@ class Main(QMainWindow, Ui_MainWindow):
 
         self.user_id = None
         self.user_pw = None
+        self.is_playing = False
+        self.youtube = None
+        self.youtube_fsize = 0
 
         self.setupUi(self)
         self.initAuth()
@@ -27,7 +31,7 @@ class Main(QMainWindow, Ui_MainWindow):
         self.previewBtn.setEnabled(False)
         self.fileNavBtn.setEnabled(False)
         self.qualityCB.setEnabled(False)
-        self.startBtn.setEnabled(False)
+        self.downloadBtn.setEnabled(False)
         self.calendarWidget.setEnabled(False)
         self.urlLE.setEnabled(False)
         self.pathLE.setEnabled(False)
@@ -42,12 +46,6 @@ class Main(QMainWindow, Ui_MainWindow):
         self.pathLE.setEnabled(True)
         self.showStatusMsg('Authenticated')
 
-    def showStatusMsg(self, msg):
-        self.statusbar.showMessage(msg)
-
-    def initSignal(self):
-        self.loginBtn.clicked.connect(self.checkAuth)
-
     @pyqtSlot()
     def checkAuth(self):
         dialog = AuthDialog()
@@ -60,8 +58,101 @@ class Main(QMainWindow, Ui_MainWindow):
             self.loginBtn.setText('Authentication Succeed')
             self.loginBtn.setEnabled(False)
             self.urlLE.setFocus(True)
+            self.appendLogMsg('Successfully login')
         else:
             QMessageBox.about(self, 'Failed to authenticate', 'Wrong ID or password')
+
+    def initSignal(self):
+        self.loginBtn.clicked.connect(self.checkAuth)
+        self.previewBtn.clicked.connect(self.loadUrl)
+        self.endBtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        self.webView.loadProgress.connect(self.showProgressBrowserLoading)
+        self.fileNavBtn.clicked.connect(self.selectDownPath)
+        self.calendarWidget.clicked.connect(self.logCalender)
+        self.downloadBtn.clicked.connect(self.downloadYoutube)
+
+    def loadUrl(self):
+        url = self.urlLE.text().strip()
+        v = re.compile('^https://www.youtube.com/?')
+        if self.is_playing:
+            self.appendLogMsg('Clicked Stop button')
+            self.webView.load(QUrl('about:blank'))
+            self.previewBtn.setText('Play')
+            self.is_playing = False
+            self.urlLE.clear()
+            self.urlLE.setFocus(True)
+            self.downloadBtn.setEnabled(False)
+            self.qualityCB.clear()
+            self.progressBar_2.setValue(0)
+            self.showStatusMsg('Authenticated')
+        else:
+            if v.match(url) is not None:
+                self.appendLogMsg('Clicked Play button')
+                self.webView.load(QUrl(url))
+                self.showStatusMsg(url + ' is playing')
+                self.previewBtn.setText('Stop')
+                self.is_playing = True
+                self.downloadBtn.setEnabled(True)
+                self.initYoutube(url)
+            else:
+                QMessageBox.about(self, 'Not available URL', 'It is not a youtube URL.')
+                self.urlLE.clear()
+                self.urlLE.setFocus(True)
+
+    def initYoutube(self, url):
+        video_list = pytube.YouTube(url)
+        video_list.register_on_progress_callback(self.showProgressDownloadLoading)
+        self.youtube = video_list.streams.all()
+        self.qualityCB.clear()
+        for q in self.youtube:
+            temp_list, str_list = [], []
+            temp_list.append(str(q.mime_type or ''))
+            temp_list.append(str(q.res or ''))
+            temp_list.append(str(q.fps or ''))
+            temp_list.append(str(q.abr or ''))
+            str_list = [x for x in temp_list if x != '']
+            self.qualityCB.addItem(', '.join(str_list))
+
+    @pyqtSlot()
+    def downloadYoutube(self):
+        down_path = self.pathLE.text().strip()
+        if down_path is None or down_path == '' or not down_path:
+            QMessageBox.about(self, 'Select Path', 'Select path to download')
+            return None
+
+        self.youtube_fsize = self.youtube[self.qualityCB.currentIndex()].filesize
+        self.youtube[self.qualityCB.currentIndex()].download(down_path)
+        self.appendLogMsg('Clicked Download button')
+
+    @pyqtSlot()
+    def selectDownPath(self):
+        fpath = QFileDialog.getExistingDirectory(self, 'Select Directory')
+        self.pathLE.setText(fpath)
+
+    @pyqtSlot()
+    def logCalender(self):
+        cur_date = self.calendarWidget.selectedDate()
+        self.appendLogMsg('Clicked calendar : ' + str(cur_date.year()) + '-' + str(cur_date.month()) + '-' + str(cur_date.day()))
+
+    @pyqtSlot(int)
+    def showProgressBrowserLoading(self, v):
+        self.progressBar.setValue(v)
+
+    @pyqtSlot(int)
+    def showProgressDownloadLoading(self, stream, chunk, file_handler, bytes_remaining):
+        self.progressBar_2.setValue(int(((self.youtube_fsize - bytes_remaining) / self.youtube_fsize) * 100))
+
+    def showStatusMsg(self, msg):
+        self.statusbar.showMessage(msg)
+
+    def appendLogMsg(self, act):
+        now = datetime.datetime.now()
+        nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+        msg =  '[' + nowDatetime + '] ' + self.user_id + ' : ' + act
+        self.logPTE.appendPlainText(msg)
+
+        with open('./log.txt', 'a') as f:
+            f.write(msg + '\n')
 
 
 if __name__ == '__main__':
